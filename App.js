52086d6a2e18cd4961bde1ba6a915d8d52efdcbe
Erdinç Coşkun <coskunerdinc06@gmail.com>
2023-05-06 13:26:48 +0300

    Version 2

--- App.js
+++ App.js
@@ -69,6 +69,7 @@ export default function App() {
       >
         <SafeAreaView style={styles.maincontainer}>{screen}</SafeAreaView>
       </ImageBackground>
+      <StatusBar style="light" />
     </LinearGradient>
   );
 }
